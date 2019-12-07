adf5f4cec72a9174df25a34d3c2c5728b5cd9034
Tim S. Biernat <tbiernat@ra.rockwell.com>
2019-12-06 22:41:24 -0600

    update vol icons, api url

--- src/app/tunes.service.ts
+++ src/app/tunes.service.ts
@@ -8,6 +8,7 @@ export interface MusicNode {
     id?: number;
     children?: MusicNode[];
 }
+const API_URL = '';
 
 @Injectable({
     providedIn: 'root',
@@ -16,27 +17,27 @@ export class TunesService {
     constructor(private http: HttpClient) { }
 
     getTunes(): Observable<MusicNode[]> {
-        return this.http.get<MusicNode[]>('http://localhost:3001/api/v1/tree');
+        return this.http.get<MusicNode[]>(API_URL + '/api/v1/tree');
     }
 
     play(id: number) {
-        return this.http.post('http://localhost:3001/api/v1/play/' + id, null).subscribe(res => { });
+        return this.http.post(API_URL + '/api/v1/play/' + id, null).subscribe(res => { });
 
     }
 
     pause() {
-        return this.http.post('http://localhost:3001/api/v1/pause', null).subscribe(res => { });
+        return this.http.post(API_URL + '/api/v1/pause', null).subscribe(res => { });
     }
 
     resume() {
-        return this.http.post('http://localhost:3001/api/v1/resume', null).subscribe(res => { });
+        return this.http.post(API_URL + '/api/v1/resume', null).subscribe(res => { });
     }
 
     stop() {
-        return this.http.post('http://localhost:3001/api/v1/stop', null).subscribe(res => { });
+        return this.http.post(API_URL + '/api/v1/stop', null).subscribe(res => { });
     }
 
     volume(level: number) {
-        return this.http.post('http://localhost:3001/api/v1/volume/' + level, null).subscribe(res => { });
+        return this.http.post(API_URL + '/api/v1/volume/' + level, null).subscribe(res => { });
     }
 }
